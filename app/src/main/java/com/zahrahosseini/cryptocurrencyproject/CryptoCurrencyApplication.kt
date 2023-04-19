fbc663488451be666e1234a11d872d087ead70fe
z.hoseini <z.hoseini@rad.holdings>
2023-04-19 20:34:41 +0330

    Initial commit

---
+++ app/src/main/java/com/zahrahosseini/cryptocurrencyproject/CryptoCurrencyApplication.kt
@@ -0,0 +1,9 @@
+package com.zahrahosseini.cryptocurrencyproject
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class CryptoCurrencyApplication : Application() {
+}
---
+++ app/src/main/java/com/zahrahosseini/cryptocurrencyproject/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zahrahosseini.cryptocurrencyproject.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zahrahosseini.cryptocurrencyproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
